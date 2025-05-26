b157e2f46514a5c2cef02815d9e6c7db18f61234
vmaxime <mister.max0303@gmail.com>
2025-05-26 11:36:30 +0200

    ajout remote que mathieu a oublié

--- ServiceDitributeur.java
+++ ServiceDitributeur.java
@@ -1,3 +1,6 @@
-public interface ServiceDitributeur {
-	public ServiceNoeud donneMachine();
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServiceDitributeur extends Remote{
+	public ServiceNoeud donneMachine() throws RemoteException;
 }
--- ServiceNoeud.java
+++ ServiceNoeud.java
@@ -1,3 +1,6 @@
-public interface ServiceNoeud {
-	public Image calculer(Scene scene, int x, int y, int w, int h);
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServiceNoeud  extends Remote{
+	public Image calculer(Scene scene, int x, int y, int w, int h) throws RemoteException;
 }
