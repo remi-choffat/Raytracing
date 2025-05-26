a376868995a402b67f9a06099d15fce2ed745fd2
vmaxime <mister.max0303@gmail.com>
2025-05-26 11:46:22 +0200

    ajout methode service noeud + implementation de service noeud

---
+++ Noeud.java
@@ -0,0 +1,28 @@
+import java.rmi.RemoteException;
+import raytracer.Scene;
+import raytracer.Image;
+
+/**
+ * Noeud
+ */
+public class Noeud implements ServiceNoeud {
+
+	private boolean libre = true;
+
+	public Image calculer(Scene scene, int x, int y, int w, int h) throws RemoteException {
+		if (this.libre) {
+			this.libre = false;
+			Image r = scene.compute(x, y, w, h);
+			this.libre = true;
+			return r;
+		} else {
+			throw new RemoteException("MACHINE OCCUPEE");
+		}
+	}
+
+	public boolean estLibre() throws RemoteException {
+		return this.libre;
+	}
+
+	
+}
--- ServiceNoeud.java
+++ ServiceNoeud.java
@@ -3,4 +3,5 @@ import java.rmi.RemoteException;
 
 public interface ServiceNoeud  extends Remote{
 	public Image calculer(Scene scene, int x, int y, int w, int h) throws RemoteException;
+	public boolean estLibre() throws RemoteException;
 }
