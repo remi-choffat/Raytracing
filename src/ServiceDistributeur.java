6499efc22d603a37b5e1be1e83be3659d4af8b5c
Rémi Choffat <remi.choffat1@etu.univ-lorraine.fr>
2025-05-27 16:31:21 +0200

    Update ServiceDistributeur.java

--- src/ServiceDistributeur.java
+++ src/ServiceDistributeur.java
@@ -6,5 +6,20 @@ import java.rmi.RemoteException;
  * Cette interface définit la méthode pour obtenir une machine de calcul.
  */
 public interface ServiceDistributeur extends Remote {
+
+    /**
+     * Enregistre un nœud de calcul dans le service distributeur.
+     *
+     * @param noeud le nœud de calcul à enregistrer
+     * @throws RemoteException si une erreur de communication RMI se produit
+     */
+    void enregistrerNoeud(ServiceNoeud noeud) throws RemoteException;
+
+    /**
+     * Donne un nœud de calcul disponible pour effectuer un raytracing.
+     *
+     * @return un objet ServiceNoeud représentant le nœud de calcul
+     * @throws RemoteException si une erreur de communication RMI se produit
+     */
     ServiceNoeud donneMachine() throws RemoteException;
 }
