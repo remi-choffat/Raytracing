d2faf4e076d0373372444f405537213b4b0e97f3
vmaxime <mister.max0303@gmail.com>
2025-05-27 16:45:59 +0200

    Noeud fini ?

--- src/Noeud.java
+++ src/Noeud.java
@@ -1,4 +1,7 @@
 import java.rmi.RemoteException;
+import java.rmi.NotBoundException;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 import raytracer.Scene;
 import raytracer.Image;
 
@@ -22,6 +25,48 @@ public class Noeud implements ServiceNoeud {
 
 	public boolean estLibre() throws RemoteException {
 		return this.libre;
+	}
+
+
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.err.println("Missing parameter, Usage -> java Noeud [Distibuteur IP] [Distibuteur Port]");
+			return;
+		} 
+		Registry reg;
+		try {
+			reg = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
+		} catch (Exception e) {
+			System.err.println("Erreur, registry pas trouve a l'adresse' " + args[0] + ":" + args[1]);
+			e.printStackTrace();
+			return;
+		}
+
+		ServiceDistributeur sd;
+
+		try {
+			sd = (ServiceDistributeur) reg.lookup("DistributeurRaytracer");
+		} catch (NotBoundException e) {
+			System.err.println("Erreur, ServiceDistributeur pas trouve dans le registry");
+			e.printStackTrace();
+			return;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		ServiceNoeud sn = new Noeud();
+
+		try {
+			sd.enregistrerNoeud(sn);
+		} catch (RemoteException e) {
+			System.err.println("Erreur a l'enregistrement du noeud dans le distributeur'");
+			e.printStackTrace();
+			return;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
 	}
 
 	
